7aacf23820ad0aabbb440822bbd0701e296668b7
Victor Casas <victor.casas@outlook.cl>
2020-11-21 13:36:37 -0300

    refactor: Semantic HTML

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import styles from '@styles/Home.module.css'
 import MetaTags from '@components/MetaTags'
+import styles from '@styles/Home.module.css'
 
 export default function Home() {
   const { pathname } = useRouter()
@@ -9,12 +9,12 @@ export default function Home() {
   const description = 'this application has been built with Next.js'
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <MetaTags url={pathname} title={title} description={description} />
       </Head>
 
-      <main className={styles.main}>
+      <div className={styles.main}>
         <h1 className={styles.title}>
           Welcome to
           <a href='https://nextjs.org'>Next.js!</a>
@@ -54,18 +54,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   )
 }
